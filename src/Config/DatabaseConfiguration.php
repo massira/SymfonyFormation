79b4324431e1d8c2e13dff3ecad62c2563f7239f
Massira <mohamed.anbri92@gmail.com>
2017-08-18 10:00:05 +0100

    Adding nodes configuration

--- src/Config/DatabaseConfiguration.php
+++ src/Config/DatabaseConfiguration.php
@@ -5,7 +5,7 @@ namespace Config\Formation\Config;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
-use Symfony\Component\Config\Definition\ConfigurationInterface
+use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 /**
  * Class DatabaseConfiguration
@@ -14,7 +14,6 @@ use Symfony\Component\Config\Definition\ConfigurationInterface
  */
 class DatabaseConfiguration implements ConfigurationInterface
 {
-
     /**
      * @inheritdoc
      */
@@ -30,14 +29,56 @@ class DatabaseConfiguration implements ConfigurationInterface
         /**Variable Nodes**/
         $rootNode //The root node is an array node and has children(auto_connect and default_connection)
             ->children()
+                //boolean node
                 ->booleanNode('auto_connect') //Pass the name of the node
                     ->defaultTrue()
                 ->end() //Returns back to the parent node
+                //scalar node
                 ->scalarNode('default_connection')
                     ->defaultValue('default')
                 ->end()
+                //Numeric(integer|float) Nodes
+                ->integerNode('positive_value')
+                    ->min(0)
+                ->end()
+                ->floatNode('big_value')
+                    ->max(5E43)
+                ->end()
+                ->integerNode('value_inside_a_range')
+                    ->min(-50)->max(50)
+                ->end()
+                //Enum Nodes => Enum nodes provide a constraint to match the given input against a set of values
+                ->enumNode('delivery')
+                    ->values(['standard', 'expedited', 'priority'])
+                ->end()
+                //Array nodes
+                ->arrayNode('connection')
+                    ->children()
+                        ->scalarNode('driver')->end()
+                        ->scalarNode('host')->end()
+                        ->scalarNode('username')->end()
+                        ->scalarNode('password')->end()
+                    ->end()
+                ->end()
             ->end();
-
+            /*
+             * -A prototype can be used to add a definition which may be repeated many times inside the current node.
+             * -According to the prototype definition in the example below, it is possible to have multiple connection
+             *  arrays (containing a driver, host, etc.).
+             */
+            $rootNode
+                ->children()
+                    ->arrayNode('connections')
+                        ->prototype('array') //Normally prototype('array') return ArrayNodeDefinition
+                            ->children()
+                                ->scalarNode('driver')->end()
+                                ->scalarNode('host')->end()
+                                ->scalarNode('username')->end()
+                                ->scalarNode('password')->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end();
 
         return $treeBuilder;
     }
@@ -45,6 +86,7 @@ class DatabaseConfiguration implements ConfigurationInterface
 
 /******Node Type*******/
 //It is possible to validate the type of a provided value by using the appropriate node definition.
+
 /*
  * 1-scalar   : Generic type that includes booleans, strings, integers, floats and null
  * 2-boolean
@@ -54,6 +96,8 @@ class DatabaseConfiguration implements ConfigurationInterface
  * 6-array
  * 7-variable : No validation
  */
+
+//Nodes are created using "$nodeBuilder->node($name, $type);" or "$nodeBuilder->xxxNode($name);"
 
 
 
